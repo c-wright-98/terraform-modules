11b9b68f594c474a4c1295b1b94077cdd57f51ec
CW <corvus.wright@gmail.com>
2023-08-30 13:21:10 +0000

    Created gitignore, removed extra output, removed typo

--- module-1/main.tf
+++ module-1/main.tf
@@ -1,13 +1,9 @@
 resource "aws_instance" "web_servers" {
   count         = var.instance_count
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0eb260c4d5475b901"
   instance_type = var.instance_type
 
   tags = {
     Name = "WebServer-${count.index}"
   }
-}
-
-output "instance_ids" {
-  value = aws_instance.web_servers[*].id
 }
